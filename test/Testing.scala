1bd853ccf976e5cb9c8c4ad6fe20fa0cfed11b21
Deshpande <adm1682@lainpuma000140.springer-sbm.com>
2015-11-09 12:32:51 +0530

    made some changes

--- test/Testing.scala
+++ test/Testing.scala
@@ -1,9 +1,11 @@
+
 import play.api.test.{FakeRequest, WithApplication, PlaySpecification}
 
 class Testing extends PlaySpecification{
 
   //Testing Controller
   "respond to the index Action" in new WithApplication{
+
     val result = controllers.Application.index()(FakeRequest())
 
     status(result) must beEqualTo(OK)
@@ -17,4 +19,28 @@ class Testing extends PlaySpecification{
     val html = views.html.main()
     contentAsString(html) must contain("Survey")
   }
+
+  "testing controller method" in new WithApplication{
+
+    val res = controllers.Application.surveyAnswers("popo")(FakeRequest())
+
+    status(res) must beEqualTo(OK)
+    contentType(res) must beSome("text/html")
+    contentAsString(res) must contain("Some answers to your questions")
+  }
+
+  "check response redirect" in new WithApplication{
+
+  //routes.Application.surveyAnswers("kls").toString() must beEqualTo("/updateQ/surveyAns/email=kls")
+
+    val request = FakeRequest(GET, "/surveyAns/email=popo")
+
+    val response = route(request).get
+
+    status(response) must be_==(OK)
+
+  //  val result = controllers.Users.surveyQuestions("kls").apply(request)
+  //  redirectLocation(result) must beSome(routes.QuestionAnswer.doneAnswering().url)
+  }
+
  }
